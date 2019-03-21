32f19df0caa645cf37d7ebd35b0873a9e2963191
Justin <justinhuang3299@gmail.com>
2019-03-21 16:51:16 -0600

    Sql Query

--- CSNet/NorthwindSystem/BLL/ProductController.cs
+++ CSNet/NorthwindSystem/BLL/ProductController.cs
@@ -5,29 +5,35 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-#region Additonal Namespaces
-using NorthwindSystem.Data;
-using NorthwindSystem.DAL;
+#region Additional Namespaces
+using NorthwindSystem.Data;     //access to data definitions
+using NorthwindSystem.DAL;      //access to context class
+using System.Data.SqlClient;    //access to SqlParameter
 #endregion
+
 namespace NorthwindSystem.BLL
 {
     //this class will be called from an external source
-    //in our example, this source will be the web page.
-    //naming standard is <T>Controller which represents a particular data class (SQL table)
+    //in our example, this source will be the web page
+    //naming standard is <T>Controller which represents
+    //    a particular data class (sql table)
     public class ProductController
     {
         //code methods which will be called for processing
         //methods will be public
         //these methods are referred to as the system interface
 
-        //a method to look up a record on the database table by primary key
+        //a method to lookup a record on the database table
+        //    by primary key
         //input: primary key value
         //output: instance of data class
         public Product Product_Get(int productid)
         {
-            //the processing of the request will be done in a transaction using the context class
+            //the processing of the request will be done
+            //  in a transaction using the Context class
             //a) instance of Context class
-            //b) issue the request for lookup via the appropriate DBSet<T>
+            //b) issue the request for lookup via the appropriate
+            //      DbSet<T>
             //c) return results
             using (var context = new NorthwindContext())
             {
@@ -35,9 +41,9 @@ namespace NorthwindSystem.BLL
             }
         }
 
-        //a method to retreive all records on the DBSet<T>
+        //a method to retreive all records on the DbSet<T>
         //input: none
-        //output: List<T>
+        //ouput: List<T>
         public List<Product> Product_List()
         {
             using (var context = new NorthwindContext())
@@ -45,5 +51,28 @@ namespace NorthwindSystem.BLL
                 return context.Products.ToList();
             }
         }
+        public List<Product> Product_GetByCategory(int categoryID)
+        {
+            using (var context = new NorthwindContext())
+            {
+                //normally you will find that data from EntityFramework returns as an IEnumerable<T> datatype
+                //one can conver the IEnumberable<T> to a List<T> using .ToList()
+                IEnumerable<Product> results = context.Database.SqlQuery<Product>("Products_GetByCategories @CategoryID", new SqlParameter("CategoryID", categoryID));
+                return results.ToList();
+            }
+        }
     }
+    
 }
+//at times you will need to do a NON PRIMARY KEY lookup
+//you will NOT be able to use .Find(pkey)
+//you can call sql procedures via your context class within your BLL class method
+//you will use .Database.SqlQuery<T>()
+//the argument(s) for SqlQuery are
+//  a) the sql procedure execute statement (as a string)
+//  b) if REQUIRED, any arguments for the procedure
+//passing the data arguments to the procedure will make use of new SqlParameter() object
+//the SqlParameter object needs a using caluse: System.Data.SqlClient
+//SqlParameter takes two arguments
+//  a) procedure parameter name
+//  b) value to be passed
--- CSNet/WebApp/SamplePages/SimpleQueries.aspx
+++ CSNet/WebApp/SamplePages/SimpleQueries.aspx
@@ -3,27 +3,38 @@
     <h1>Simple Queries</h1>
     <table align="center" style="width: 80%">
         <tr>
-            <td>
+            <td  align="right">
                 <asp:Label ID="Label1" runat="server" Text="Enter Product ID:"></asp:Label>
+                &nbsp;&nbsp;
                 <asp:TextBox ID="SearchArg" runat="server"></asp:TextBox>
             </td>
             <td>
                 <asp:Label ID="Label2" runat="server" Text="Product ID:"></asp:Label>
-                <asp:Label ID="ProductID" runat="server"></asp:Label>
+                 &nbsp;&nbsp;
+                <asp:Label ID="ProductID" runat="server" ></asp:Label>
             </td>
         </tr>
         <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
+            <td  align="right">
+                <asp:Button ID="Submit" runat="server" Text="Submit" OnClick="Submit_Click" />
+                 &nbsp;&nbsp;
+                <asp:Button ID="Clear" runat="server" Text="Clear" CausesValidation="false" OnClick="Clear_Click" />
+            </td>
+            <td>
+                <asp:Label ID="Label4" runat="server" Text="Name:"></asp:Label>
+                 &nbsp;&nbsp;
+                <asp:Label ID="ProductName" runat="server" ></asp:Label>
+            </td>
         </tr>
         <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
+            <td></td>
+            <td></td>
         </tr>
         <tr>
             <td colspan="2" align="center">
-                <asp:Label ID="MessageLabel" runat="server" Text="Message"></asp:Label></td>
-            <td>&nbsp;</td>
+                <asp:Label ID="MessageLabel" runat="server" ></asp:Label>
+            </td>
+            
         </tr>
     </table>
 </asp:Content>
