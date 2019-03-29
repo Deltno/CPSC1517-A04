6935e8d8f718711fa23817b3e59c258dc9188e37
Justin <justinhuang3299@gmail.com>
2019-03-29 10:19:22 -0600

    Update missed classes

--- CSNet/NorthwindSystem/BLL/ProductController.cs
+++ CSNet/NorthwindSystem/BLL/ProductController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 #region Additional Namespaces
 using NorthwindSystem.Data;     //access to data definitions
 using NorthwindSystem.DAL;      //access to context class
-using System.Data.SqlClient;    //access to SqlParameter
+using System.Data.SqlClient;    //access to SqlParameter()
 #endregion
 
 namespace NorthwindSystem.BLL
@@ -51,28 +51,91 @@ namespace NorthwindSystem.BLL
                 return context.Products.ToList();
             }
         }
-        public List<Product> Product_GetByCategory(int categoryID)
+    
+
+        //at times you will need to do a NON-PRIMARY KEY lookup
+        //you will NOT be able to use .Find(pkey)
+        //you can call sql procedures via your context class
+        //    within your bll class method
+        //use will use .Database.SqlQuery<T>()  NOT the DbSet<T>
+        //the argument(s) for SqlQuery are
+        // a) the sql procedure execute statement (as a string)
+        // b) IF REQUIRED, any arguments for the procedure
+        //passing the data arguments to the procedure will make use of
+        //      new SqlParameter() object
+        //the SqlParameter object needs a using clause: System.Data.SqlClient
+        //SqlParameter takes two arguments
+        // a) procedure parameter name
+        // b) value to be passed
+        public List<Product> Product_GetByCategory(int categoryid)
         {
             using (var context = new NorthwindContext())
             {
-                //normally you will find that data from EntityFramework returns as an IEnumerable<T> datatype
-                //one can conver the IEnumberable<T> to a List<T> using .ToList()
-                IEnumerable<Product> results = context.Database.SqlQuery<Product>("Products_GetByCategories @CategoryID", new SqlParameter("CategoryID", categoryID));
+                //normally you will find that data from EntityFramework
+                //     returns as an IEnumerable<T> datatype
+                //one can convert the IEnumerable<T> to a List<T> using .ToList()
+                IEnumerable<Product> results =
+                    context.Database.SqlQuery<Product>(
+                        "Products_GetByCategories @CategoryID",
+                        new SqlParameter("CategoryID", categoryid));
                 return results.ToList();
             }
         }
+        public List<Product> Products_GetByPartialProductName(string partialname)
+        {
+            using (var context = new NorthwindContext())
+            {
+                IEnumerable<Product> results =
+                    context.Database.SqlQuery<Product>("Products_GetByPartialProductName @PartialName",
+                                    new SqlParameter("PartialName", partialname));
+                return results.ToList();
+            }
+        }
+
+      
+
+        public List<Product> Products_GetBySupplierPartialProductName(int supplierid, string partialproductname)
+        {
+            using (var context = new NorthwindContext())
+            {
+                //sometimes there may be a sql error that does not like the new SqlParameter()
+                //       coded directly in the SqlQuery call
+                //if this happens to you then code your parameters as shown below then
+                //       use the parm1 and parm2 in the SqlQuery call instead of the new....
+                //don't know why but its weird
+                //var parm1 = new SqlParameter("SupplierID", supplierid);
+                //var parm2 = new SqlParameter("PartialProductName", partialproductname);
+                IEnumerable<Product> results =
+                    context.Database.SqlQuery<Product>("Products_GetBySupplierPartialProductName @SupplierID, @PartialProductName",
+                                    new SqlParameter("SupplierID", supplierid),
+                                    new SqlParameter("PartialProductName", partialproductname));
+                return results.ToList();
+            }
+        }
+
+        public List<Product> Products_GetForSupplierCategory(int supplierid, int categoryid)
+        {
+            using (var context = new NorthwindContext())
+            {
+                IEnumerable<Product> results =
+                    context.Database.SqlQuery<Product>("Products_GetForSupplierCategory @SupplierID, @CategoryID",
+                                    new SqlParameter("SupplierID", supplierid),
+                                    new SqlParameter("CategoryID", categoryid));
+                return results.ToList();
+            }
+        }
+
+        public List<Product> Products_GetByCategoryAndName(int category, string partialname)
+        {
+            using (var context = new NorthwindContext())
+            {
+                IEnumerable<Product> results =
+                    context.Database.SqlQuery<Product>("Products_GetByCategoryAndName @CategoryID, @PartialName",
+                                    new SqlParameter("CategoryID", category),
+                                    new SqlParameter("PartialName", partialname));
+                return results.ToList();
+            }
+        }
+
     }
-    
 }
-//at times you will need to do a NON PRIMARY KEY lookup
-//you will NOT be able to use .Find(pkey)
-//you can call sql procedures via your context class within your BLL class method
-//you will use .Database.SqlQuery<T>()
-//the argument(s) for SqlQuery are
-//  a) the sql procedure execute statement (as a string)
-//  b) if REQUIRED, any arguments for the procedure
-//passing the data arguments to the procedure will make use of new SqlParameter() object
-//the SqlParameter object needs a using caluse: System.Data.SqlClient
-//SqlParameter takes two arguments
-//  a) procedure parameter name
-//  b) value to be passed
--- CSNet/WebApp/SamplePages/SqlProcQueries.aspx
+++ CSNet/WebApp/SamplePages/SqlProcQueries.aspx
@@ -23,7 +23,49 @@
             </tr>
             <tr>
                 <td align="center">
-                    <asp:GridView ID="CategoryProductList" runat="server"></asp:GridView>
+                    <asp:GridView ID="CategoryProductList" runat="server" AutoGenerateColumns="False" CellPadding="5" CellSpacing="5" GridLines="Horizontal" BorderStyle="None" AllowPaging="True" OnPageIndexChanging="CategoryProductList_PageIndexChanging" PageSize="3" OnSelectedIndexChanged="CategoryProductList_SelectedIndexChanged">
+                        <AlternatingRowStyle BackColor="#CCCCCC" />
+                        <Columns>
+                             <asp:TemplateField Visible="False" >
+                                <ItemTemplate>
+                                    <asp:Label ID="ProductID" runat="server" Text='<%# Eval("ProductID") %>'></asp:Label>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="Product">
+                                <ItemTemplate>
+                                    <asp:Label ID="ProductName" runat="server" Text='<%# Eval("ProductName") %>'></asp:Label>
+                                </ItemTemplate>
+                                <HeaderStyle BackColor="#99CCFF" Font-Bold="True" />
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="Price ($)">
+                                 <ItemTemplate>
+                                    <asp:Label ID="UnitPrice" runat="server" 
+                                        Text='<%# string.Format("{0:0.00}",Eval("UnitPrice")) %>'></asp:Label>
+                                </ItemTemplate>
+                                <HeaderStyle BackColor="#99CCFF" Font-Bold="True" />
+                                 <ItemStyle HorizontalAlign="Right" />
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="QoH">
+                                 <ItemTemplate>
+                                    <asp:Label ID="UnitsInStock" runat="server" Text='<%# Eval("UnitsInStock") %>'></asp:Label>
+                                </ItemTemplate>
+                                <HeaderStyle BackColor="#99CCFF" Font-Bold="True" />
+                                 <ItemStyle HorizontalAlign="Right" />
+                            </asp:TemplateField>
+                            <asp:TemplateField HeaderText="Disc">
+                                 <ItemTemplate>
+                                     <asp:CheckBox ID="Discontinued" runat="server"
+                                          Checked='<%# Eval("Discontinued")%>'/>
+                                </ItemTemplate>
+                                <HeaderStyle BackColor="#99CCFF" Font-Bold="True" />
+                                 <ItemStyle HorizontalAlign="Center" />
+                            </asp:TemplateField>
+                             <asp:CommandField CausesValidation="False" SelectText="View" ShowSelectButton="True">
+                             <HeaderStyle BackColor="#99CCFF" />
+                             </asp:CommandField>
+                        </Columns>
+                        <PagerSettings FirstPageText="Start" LastPageText="End" Mode="NumericFirstLast" PageButtonCount="3" />
+                    </asp:GridView>
                 </td>
 
             </tr>
